0f4cdb5c66bfa5bdac33d55c410ef1310e21f0fe
Christopher Nilsson <christopher.nille.nilsson@gmail.com>
2019-02-26 13:03:06 +0100

    feat: Implement double lists

--- src/App.js
+++ src/App.js
@@ -4,13 +4,58 @@ import './App.css';
 import List from './List';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      items: []
+    }
+  }
+
+  addTodo = (name) => {
+    const { items } = this.state;
+    const newItem = {
+      name,
+      done: false,
+      key: items.length
+    }
+    items.push(newItem)
+    this.setState({ items });
+  }
+
+  toggleCheckBox = (idx) => {
+    const { items } = this.state;
+    const item = items[idx];
+    items[idx].done = !items[idx].done;
+    this.setState({
+      items
+    })
+  }
   render() {
+    const { items } = this.state;
+
+    const doneItems = items.filter(i => i.done)
+    const todoItems = items.filter(i => !i.done)
+
     return (
       <div className="App">
         <header className="App-header">
           <p className="App-logo">🧐</p>
-          <List name="My Todo List" />
-          <List name="Done Todos" />
+          <List
+            name="My Todo List"
+            moveToDone={this.moveToDone}
+            addTodo={this.addTodo}
+            items={todoItems}
+            toggleCheckBox={this.toggleCheckBox}
+          />
+          <List
+            name="Done List"
+            moveToTodo={this.moveToTodo}
+            isDoneList
+            items={doneItems}
+            toggleCheckBox={this.toggleCheckBox}
+          />
         </header>
       </div>
     );
