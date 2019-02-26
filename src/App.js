251c5b9d792b0cd5cecf4a85327a80c5a31104a6
Christopher Nilsson <christopher.nille.nilsson@gmail.com>
2019-02-26 12:08:51 +0100

    make logo more fun

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <p className="App-logo">🧐</p>
           <List name="My Todo List" />
         </header>
       </div>
